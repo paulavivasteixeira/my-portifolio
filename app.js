2f81f209da52e4c3a4aa573e12f38f4c6881ba2b
paulavivas <paulavivasteixeira@gmail.com>
2022-10-24 20:04:11 -0400

    added responsive nav bar + new styles

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-// Responsive nav bar
+const hamburgerBtn = document.querySelector(".navToggler");
+const navMenu = document.querySelector(".navMenu");
 
+hamburgerBtn.addEventListener("click", () => {
+  hamburgerBtn.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll('.navItem').forEach(n => n.addEventListener("click", () => {
+  hamburgerBtn.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
